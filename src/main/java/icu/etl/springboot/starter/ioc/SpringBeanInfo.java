5f874a588acc917da98f2397d70d4ef86ecbcdb5
jeremy8551 <jeremy8551@gamil.com>
2023-10-27 15:32:32 +0800

    临时

--- src/main/java/icu/etl/springboot/starter/ioc/SpringBeanInfo.java
+++ src/main/java/icu/etl/springboot/starter/ioc/SpringBeanInfo.java
@@ -1,13 +1,13 @@
 package icu.etl.springboot.starter.ioc;
 
-import icu.etl.ioc.AnnotationBeanInfo;
+import icu.etl.ioc.AnnotationBeanInfoRegister;
 import org.springframework.context.ApplicationContext;
 
 /**
  * @author jeremy8551@qq.com
  * @createtime 2023/10/26
  */
-public class SpringBeanInfo extends AnnotationBeanInfo {
+public class SpringBeanInfo extends AnnotationBeanInfoRegister {
 
     /**
      * 初始化
@@ -15,6 +15,6 @@ public class SpringBeanInfo extends AnnotationBeanInfo {
     public SpringBeanInfo(ApplicationContext springContext) {
         super(ApplicationContext.class);
         this.singleton = true;
-        this.setInstance(springContext);
+        this.setBean(springContext);
     }
 }
